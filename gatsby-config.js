e1643407973379789a93a26c35544d132df6375c
matildaIE <matilda.rutherford@ie.com.au>
2020-03-31 07:58:23 +1100

    rename

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,42 @@
 const path = require('path');
 
 module.exports = {
-  siteMetadata: {
-    title: 'Baby | Face',
-    description: 'IE Baby Face Competition'
-  },
-  plugins: [
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-json',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: path.join(__dirname, 'src', 'data'),
-        name: 'data',
+   siteMetadata: {
+      title: 'WF | Who?',
+      description: 'IE WF-Who? Competition'
+   },
+   plugins: [
+      'gatsby-transformer-sharp',
+      'gatsby-plugin-sharp',
+      'gatsby-transformer-json',
+      {
+         resolve: 'gatsby-source-filesystem',
+         options: {
+            path: path.join(__dirname, 'src', 'data'),
+            name: 'data',
+         },
       },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: path.join(__dirname, 'src', 'images'),
-        name: 'images',
+      {
+         resolve: 'gatsby-source-filesystem',
+         options: {
+            path: path.join(__dirname, 'src', 'images'),
+            name: 'images',
+         },
       },
-    },
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'baby-face',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#000',
-        theme_color: '#000',
-        display: 'minimal-ui'
+      'gatsby-plugin-react-helmet',
+      {
+         resolve: `gatsby-plugin-manifest`,
+         options: {
+            name: 'wf-who',
+            short_name: 'starter',
+            start_url: '/',
+            background_color: '#000',
+            theme_color: '#000',
+            display: 'minimal-ui'
+         },
       },
-    },
-    'gatsby-plugin-offline',
-    'gatsby-plugin-styled-components',
-    'gatsby-plugin-netlify',
-  ],
+      'gatsby-plugin-offline',
+      'gatsby-plugin-styled-components',
+      'gatsby-plugin-netlify',
+   ],
 }
